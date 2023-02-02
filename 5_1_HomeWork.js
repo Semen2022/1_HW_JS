f1dc4a3985305b14d87834d4d42b70d3bc76320f
Semen2022 <study2022@mail.ru>
2023-02-02 22:04:37 +0300

    Введена проверка на а!=0

--- 5_1_HomeWork.js
+++ 5_1_HomeWork.js
@@ -1,19 +1,24 @@
 "use strict"
 
-let a = 3;
-let b = 4;
-let c = 5;
+let a = -3;
+let b = 5;
+let c = 6;
 
 function solveEquation(in_a, in_b, in_c) {
     let discriminant = in_b ** 2 - 4 * in_a * in_c;
     let result_array = [];
 
-    if (discriminant == 0) {
-        result_array = - in_b / (2 * in_a);
-    } else if (discriminant > 0) {
-        result_array = [(- in_b - Math.sqrt(discriminant)) / (2 * in_a), (- in_b + Math.sqrt(discriminant)) / (2 * in_a),];
+    // console.log(discriminant, in_b ** 2);
+    if (in_a !== 0) {
+        if (discriminant == 0) {
+            result_array = [- in_b / (2 * in_a)];
+        } else if (discriminant > 0) {
+            result_array = [(- in_b - Math.sqrt(discriminant)) / (2 * in_a), (- in_b + Math.sqrt(discriminant)) / (2 * in_a),];
+        }
+        return result_array;
+    } else {
+        return "Задайте ненулевой коэффициент \'а\'";
     }
-    return result_array;
 }
 
 console.log( solveEquation(a, b, c) );
